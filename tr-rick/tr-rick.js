12e510650c314f667df99065bb65d6aa61c3a8bf
Travis Reeder <treeder@gmail.com>
2022-01-19 15:41:21 -0500

    autoplay

--- tr-rick/tr-rick.js
+++ tr-rick/tr-rick.js
@@ -18,7 +18,7 @@ export class Rick extends LitElement {
 
     render() {
         //- can let user apply styles like this: https://lit.dev/docs/components/styles/#dynamic-classes-and-styles
-        return html`<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+        return html`<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
     }
 }
 
