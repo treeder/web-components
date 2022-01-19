245e2094422a8a03e1d1c89008914073fcfadae4
Travis Reeder <treeder@gmail.com>
2022-01-19 10:10:07 -0500

    Update tr-shadow-box.js

--- tr-shadow-box/tr-shadow-box.js
+++ tr-shadow-box/tr-shadow-box.js
@@ -1,6 +1,6 @@
 import { html, css, LitElement } from 'https://cdn.jsdelivr.net/npm/lit@2/+esm' // 'https://cdn.skypack.dev/lit'
 
-export class RaisedBox extends LitElement {
+export class ShadowBox extends LitElement {
 
     static styles = css`
         a{
@@ -9,7 +9,7 @@ export class RaisedBox extends LitElement {
         .header {
             text-align: center;
         }
-        .raisedbox { 
+        .raised-box { 
             padding: 10px;
             border: 1px solid #77aaff;
             border-radius: 6px;
@@ -18,45 +18,21 @@ export class RaisedBox extends LitElement {
         `
 
     static get properties() {
-        return {
-            // inputs:
-            userID: { type: String },
-            symbol: { type: String },
-
-            // internal:
-            signedIn: { type: Boolean },
-            balance: { type: Object },
-            fetching: { type: Boolean },
-            error: { type: Object },
-        }
+        return {}
     }
 
     constructor() {
         super();
-        this.userID = '';
-        this.symbol = '';
-
-        this.balance = {};
-
-        this.fetching = false;
-        this.error = null;
-        this.signedIn = false;
     }
 
     connectedCallback() {
         super.connectedCallback();
-        this.fetchData();
-    }
-
-    async fetchData() {
-
     }
 
     render() {
         //- can let user apply styles like this: https://lit.dev/docs/components/styles/#dynamic-classes-and-styles
-
-        return html`<div class="raisedbox"><slot></slot></div>`
+        return html`<div class="raised-box"><slot></slot></div>`
     }
 }
 
-customElements.define('raised-box', RaisedBox);
+customElements.define('tr-shadow-box', ShadowBox);
