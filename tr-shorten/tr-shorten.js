1c9cc54fd8b4d1d495d235e900ff2db2cbc91d0c
Travis Reeder <treeder@gmail.com>
2022-05-05 14:32:04 -0700

    Create tr-shorten.js

---
+++ tr-shorten/tr-shorten.js
@@ -0,0 +1,45 @@
+import { html, css, LitElement } from 'https://cdn.jsdelivr.net/npm/lit@2/+esm'
+
+export class Shorten extends LitElement {
+
+    static get styles() {
+        return css`a { color: rgb(13, 169, 239) }`;
+    }
+
+    static get properties() {
+        return {
+            value: { type: String },
+            prefixAmount: { type: Number },
+            suffixAmount: { type: Number },
+        }
+    }
+
+    constructor() {
+        super();
+        this.value = '';
+        this.prefixAmount = 4;
+        this.suffixAmount = 4;
+    }
+
+    render() {
+        let s = this.value;
+        if (this.value.length > this.prefixAmount) {
+            s = this.truncate(this.value, this.prefixAmount)
+            if (this.suffixAmount > 0) {
+                let remainder = this.text.substr(this.prefixAmount, this.value.length - 1)
+                if (remainder.length > this.suffixAmount) {
+                    remainder = remainder.substr(remainder.length - this.suffixAmount)
+                }
+                s += remainder;
+            }
+        }
+        return html`${s}`;
+    }
+
+    truncate(str, n) {
+        return (str.length > n) ? str.substr(0, n) + '...' : str;
+    }
+
+}
+
+customElements.define('tr-shorten', Shorten)
