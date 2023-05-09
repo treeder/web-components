65467d9ab07690d1272a0b077ab742f7e7b9dd2a
Travis Reeder <treeder@gmail.com>
2023-05-08 20:41:15 -0700

    Create nostrap.js

---
+++ css/nostrap.js
@@ -0,0 +1,101 @@
+import { css } from 'https://cdn.jsdelivr.net/npm/lit@2/+esm'
+
+/* 
+trying to keep consistent with: https://getbootstrap.com/docs/5.0/utilities/spacing/#margin-and-padding
+ */
+export const sharedStyles = css`
+          .mobile {
+               /* display: none; */
+          }
+
+          @media screen and (max-width: 767px) {
+               /* Use this to hide and show elements for desktop vs mobile */
+               /* .mobile {
+                    display: block;
+               } */
+
+               .desktop {
+                    display: none;
+               }
+
+               .flexr {
+                    /* for flex reponsive */
+                    flex-direction: column;
+               }
+          }
+
+          @media screen and (min-width: 768px) {
+               /* Use this to hide and show elements for desktop vs mobile */
+               .mobile {
+                    display: none;
+               }
+
+               .desktop {
+                    display: unset;
+               }
+
+               .flexr {
+                    /* for flex reponsive */
+                    flex-direction: row;
+               }
+          }
+          
+     .flex {
+    display: flex;
+  }
+  .flexw {
+    display: flex;
+    flex-wrap: wrap;
+  }
+    .flexc {
+     display: flex;
+     align-items: center;
+     justify-content: center;
+   }
+
+    .mt-3 {
+        margin-top: 1rem;
+    }
+    .mb-1 {
+          margin-bottom: 0.25rem;
+    }
+    .mb-2 {
+        margin-bottom: 0.5rem;
+   }
+   .mb-3 {
+        margin-bottom: 1rem;
+   }
+   .pt-1 {
+      padding-top: 0.25rem;
+   }
+   .pt-2 {
+      padding-top: 0.5rem;
+   }
+   .pt-3 {
+        padding-top: 1rem;
+   }
+   .pb-3 {
+        padding-bottom: 1rem;
+   }
+    .p-1 {
+    padding: 0.25rem;
+  }
+  .p-2 {
+    padding: 0.5rem;
+  }
+  .p-3 {
+    padding: 1rem;
+  }
+  .p-4 {
+    padding: 1.5rem;
+  }
+     
+   .fw {
+        width: 100%;
+   }
+   
+          .small { /* https://developer.mozilla.org/en-US/docs/Web/HTML/Element/small#css_alternative */
+               font-size: 0.8em;
+          }
+
+    `
