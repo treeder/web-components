8bd506439c7f6bcfeeef2801a65b15ce1be79fb8
Travis Reeder <treeder@gmail.com>
2023-08-21 22:50:29 -0700

    Update firebase2.js

--- firebase/firebase2.js
+++ firebase/firebase2.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js'
-import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js'
+import { getAuth, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js'
 import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js"
 import { getCookie, setCookie } from 'https://cdn.jsdelivr.net/gh/treeder/web-components@0/js/cookies.js'
 
